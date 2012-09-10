976ade92ab973bfeeb2081db250bee520d1452a3
rc <changaboy24@gmail.com>
2012-09-10 13:29:21 -0500

    depot scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '59733a0d5dffa511396c9fbcbf980c0a47d563a1d47b723fc93769e4fe1ca33db897f3daaca17247d076b68655ea51d499e1e5354ff860eb08e320acf5f1611b'
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Product.delete_all
+
+Product.create(title: 'Programming Ruby',
+				description: %{<p> What am I writing</p>},
+				image_url: 'ruby.jpg',
+				price: 49.95)
